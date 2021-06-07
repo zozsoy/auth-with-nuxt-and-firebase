56b1a07f0af42e58c33aef1426a95e550c039026
Ziya ÖZSOY <ziya@omegabigdata.com>
2021-06-07 11:37:22 +0100

    Navbar of anonymous user created both for layout and components

---
+++ layouts/anonymousUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <NavbarAnonymousUser />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import NavbarAnonymousUser from "../components/NavbarAnonymousUser"
+
+export default {
+  components: {
+    NavbarAnonymousUser,
+  },
+}
+</script>
+
+
+<style>
+
+</style>
