6fadb6c97a92d243cb684efea5d3a1977bc7e0a5
Ziya ÖZSOY <ziya@omegabigdata.com>
2021-06-07 11:30:05 +0100

    Firebase sign-in and sign-up operations are done in vuex.

---
+++ store/users/index.js
@@ -0,0 +1,42 @@
+import {auth} from "@/services/firebase"
+import Cookie from "js-cookie"
+
+export const state = () => ({
+    user : null,
+
+})
+
+export const mutations = {
+    saveUser(state, userData) {
+        this.state.users.user = userData;
+    }
+}
+
+export const actions = {
+    async createUser({commit}, userData) {
+        try {
+            await auth.createUserWithEmailAndPassword(userData.email, userData.password);
+            const {email, uid} = auth.currentUser;
+            const token = await auth.currentUser.getIdToken()
+            Cookie.set("access_token", token);
+            commit("saveUser", {email, uid});
+            console.log(auth);
+        } catch (error) {
+            throw error;
+        }
+    },
+    async singIn({commit}, userData) {
+        try {
+            await auth.signInWithEmailAndPassword(userData.email, userData.password);
+
+            const {email, uid} = auth.currentUser;
+            const token = await auth.currentUser.getIdToken()
+
+            Cookie.set("access_token", token);
+            commit("saveUser", {email, uid});
+            //console.log(auth);
+        } catch (error) {
+            throw error;
+        }
+    }
+}
