0f57d4857d2e15afa0279f7090e3901594c64caa
Ziya ÖZSOY <ziya@omegabigdata.com>
2021-06-07 11:32:15 +0100

    login (sign in and sign up) pages created

---
+++ pages/login/singup.vue
@@ -0,0 +1,70 @@
+<template>
+  
+<div class="container">
+  <b-form @submit.stop.prevent>
+    <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+    >
+    <b-form-input
+        id="input-1"
+        v-model="form.email"
+        type="email"
+        placeholder="Enter email"
+        required
+        :state="validEmail(form.email)"
+    ></b-form-input>
+    </b-form-group>
+
+    <label for="text-password">Password</label>
+    <b-form-input :state="validPassword(form.password)" v-model="form.password" type="password" id="text-password" aria-describedby="password-help-block"></b-form-input>
+    <b-form-text id="password-help-block">
+      Your password must be 8-20 characters long
+    </b-form-text>
+    <b-button @click="singup" variant="outline-primary">Sing up</b-button>
+   </b-form>
+</div>
+
+</template>
+
+<script>
+export default {
+    layout: "anonymousUser",
+    data() {
+        return {
+            form: {
+                email: "",
+                password: ""
+            }
+        }
+    },
+    methods: {
+        singup() {
+            this.$store.dispatch("users/createUser", this.form)
+            .then(() => {
+                this.$router.push("/profile")
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        },
+        validEmail: function (email) {
+            var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email);
+        },
+        validPassword: function (password) {
+            var regex = /.{8,20}/;
+            return regex.test(password);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    width: 40%;
+
+}
+</style>
